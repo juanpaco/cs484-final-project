76db257eb174efc50d163ee31df0d7b34e7b1d10
Ethan Garofolo <juanpaco@gmail.com>
2015-12-09 05:10:42 -0700

    Generator of dummy data

--- extractor/generate.rb
+++ extractor/generate.rb
@@ -1,13 +1,17 @@
-require 'faker'
-
 destination_directory = "data"
 
-100.times do |i|
+# Yanked this from the Faker gem.  We can't seem to install gems on the open
+# labs, so I just grabbed the function that we need.
+def characters(char_count = 255)
+  rand(36**char_count).to_s(36).rjust(char_count, '0').chars.to_a.shuffle.join
+end
+
+1000.times do |i|
   outfile = "#{destination_directory}/#{i}.dummy.txt"
 
   File.open(outfile, "w") do |f|
-    1000.times do |who_cares|
-      f.puts Faker::Lorem.characters(1024)
+    5000.times do |who_cares|
+      f.puts characters(1024)
     end
   end
 end
